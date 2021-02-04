23b3e5990659b78d7294ef36bbe2935bd7072055
janrutger <janrutger@jaruku.nl>
2021-02-04 20:28:49 +0100

    added pandas

--- station.py
+++ station.py
@@ -1,9 +1,13 @@
 
 import datastore as ds
 from sensehatb import Board
-from datetime import datetime
 
+#from datetime import datetime
+import pandas as pd
+#import matplotlib.pyplot as plt 
  
+
+#sample = (sample_id, station_id, parameter, time_at, time_for, value, value1, value2, units)
 
 def main():
     hat = Board()
@@ -18,8 +22,13 @@ def main():
         samples.store_sample(station_id, parameter, value, units)
         
     samples = samples.read_all_samples()
-    for sample in samples:
-        print(sample)
+
+    # for sample in samples:
+    #     print(sample)
+
+    df = pd.DataFrame(samples, columns =['sample_id', 'station_id', 'parameter', 'time_at', 'time_for', 'values', 'units']) 
+    df = df.set_index('sample_id')
+    print(df)
 
 
 
