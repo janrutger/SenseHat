b4339f756e04813e29d4fcbe20c77eff74f3f90d
janrutger <janrutger@jaruku.nl>
2021-02-05 20:46:28 +0100

    First sensors

--- sensehatb.py
+++ sensehatb.py
@@ -21,11 +21,15 @@ class  Board:
         self.station_id = "station1"
 
         self.cpu = CPUTemperature()
-        #self.sht = adafruit_shtc3.SHTC3(self.i2c)
+        self.sht = adafruit_shtc3.SHTC3(self.i2c)
+        self.lps = adafruit_lps2x.LPS22(self.i2c, 0x5c)
+        self.tcs = adafruit_tcs34725.TCS34725(self.i2c)
 
     def config(self, part):
         if part == "parameters":
-            return(["temperatureCPU", "temperature"])
+            return(["temperatureCPU", "temperature", "Humidity",
+            "temperature2", "pressure",
+            "lux", "color-temparature", "color-rgb"])
         if part == "name":
             return(self.station_id)
 
@@ -35,7 +39,30 @@ class  Board:
             unit = "C"
             return(value, unit)
         elif parameter == "temperature":
-            value = (37.4, 8, 45)
-            #value_ = self.sht.temperature
+            value = (self.sht.temperature,)
             unit = "C"
+            return(value, unit)
+        elif parameter == "Humidity":
+            value = (self.sht.relative_humidity,)
+            unit = "%"
+            return(value, unit)
+        elif parameter == "temperature2":
+            value = (self.lps.temperature,)
+            unit = "C"
+            return(value, unit)
+        elif parameter == "pressure":
+            value = (self.lps.pressure,)
+            unit = "hPa"
+            return(value, unit)
+        elif parameter == "lux":
+            value = (self.tcs.lux,)
+            unit = "lux"
+            return(value, unit)
+        elif parameter == "color-temparature":
+            value = (self.tcs.color_temperature,)
+            unit = "K"
+            return(value, unit)
+        elif parameter == "color-rgb":
+            value = (self.tcs.color_rgb_bytes)
+            unit = "RGB"
             return(value, unit)
