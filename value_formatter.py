c45e2dc8262a4a5bc16f18ef2c0fc697b4113d03
janrutger <janrutger@jaruku.nl>
2021-03-18 16:12:56 +0100

    value formatter

---
+++ value_formatter.py
@@ -0,0 +1,19 @@
+
+
+
+#"temperatureCPU", "temperature", "Humidity",
+# "temperature2", "pressure",
+# "lux", "color-temperature", "color-rgb",
+# "acceleration", "gyro", "magnetic",
+# "external1", "external2", "external3", "external4"
+
+class Formatter:
+    def __init__(self):
+        temperature = ["temperatureCPU", "temperature", "temperature2"]
+
+
+    def format(self, paraameter, value, value1, value2):
+        if paraameter in temperature:
+            symbol = "T"
+            value  = {symbol : value}
+            return(value)
